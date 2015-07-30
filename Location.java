e156dae6449b66917ece8220610be6d5e80851d3
James Barfoote <james.barfoote@gmail.com>
2015-07-30 21:35:13 +1200

    Basics

--- Location.java
+++ Location.java
@@ -1,4 +1,15 @@
 
-public class Location {
 
+public class Location {
+	Player player;
+	int x = 0;
+	int y = 0;
+	
+ public Location(Player p, int x, int y)
+ {
+	 this.player = p;
+	 this.x = x;
+	 this.y = y;
+ }
 }
+
