d68dd33613feaffbe50d071f7eb4b71fa4b600c9
CombuskenKid <Cameronporter95@gmail.com>
2015-07-30 20:07:56 +1200

    Started Program

---
+++ Weapon.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Weapon implements Card{
+
+	private String[] weapons = {"candlestick", "dagger", "revolver", "rope", "lead pipe", "spanner"};
+	
+	public Weapon(){
+		
+	}
+}
