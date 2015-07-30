44a826e856b865115ed1147d51898400e0985c9a
James Barfoote <james.barfoote@gmail.com>
2015-07-30 21:33:21 +1200

    Basics

--- Guess.java
+++ Guess.java
@@ -1,4 +1,34 @@
 
-public class Guess {
+import java.util.ArrayList;
 
+public class Guess {
+private ArrayList<Card> cards = new ArrayList<Card>();
+
+	public Guess(Boolean type, ArrayList<Card> c)
+	{
+		this.cards = c;
+		if(type)
+		{
+			Guess();
+		}
+		else
+		{
+			Answer();
+		}
+	}
+	
+	public void Guess()
+	{
+		//Iterate over the players in order
+		//Check if a player has one of the guessed cards
+		//If they do then return it and print it
+		//Then end turn
+	}
+	
+	public void Answer()
+	{
+		//Check each of the cards the player guessed against the answer cards
+		//If correct then play p wins. Answer is printed
+		//Else they are eliminated and their turn ends
+	}
 }
