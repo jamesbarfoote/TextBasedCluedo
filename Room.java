11b339c37db5bdc330ec78786bc2889a340e6944
CombuskenKid <Cameronporter95@gmail.com>
2015-07-30 21:32:40 +1200

    Sigh

--- Room.java
+++ Room.java
@@ -2,8 +2,22 @@
 public class Room {
 
 	private String[] rooms = {"Kitchen", "Ballroom", "Conservatory", "Billiard Room", "Library", "Study", "Hall", "Lounge", "Dining Room"};
+	private String name;
+	private int width, height;
 	
-	public Room(int width, int height, ){
+	public Room(String name, int width, int height){
+		this.name = name;
+		this.width = width;
+		this.height = height;
+	}
+	
+	/**
+	 * Calculates whether or not the given location in within the bounds of the room.
+	 * 
+	 * @param Location
+	 * @Return Boolean
+	 */
+	public boolean contains(Location l){
 		
 	}
 }
