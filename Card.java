02cd43c6eb10f131858d61678b4944a6c041442b
CombuskenKid <Cameronporter95@gmail.com>
2015-07-31 13:52:13 +1200

    Cleaned up Card a little bit

--- Card.java
+++ Card.java
@@ -2,23 +2,8 @@
 public abstract class Card {
 	
 	public boolean equals(Card card){
-		if(card instanceof Room){
-			Room room = (Room) card;
-			if(room.getName().equals(this.getName())){
-				return true;
-			}
-		}
-		else if(card instanceof Weapon){
-			Weapon weapon = (Weapon) card;
-			if(weapon.getName().equals(this.getName())){
-				return true;
-			}
-		}
-		else if(card instanceof Character){
-			Character character = (Character) card;
-			if(character.getName().equals(this.getName())){
-				return true;
-			}
+		if(card.getName().equals(this.getName())){
+			return true;
 		}
 		return false;
 	}
