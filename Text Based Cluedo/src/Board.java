9c582538ef88b085bf1dd5fa41384136245028ae
James Barfoote <james.barfoote@gmail.com>
2015-08-01 15:37:37 +1200

    base generate methods for board

--- Text Based Cluedo/src/Board.java
+++ Text Based Cluedo/src/Board.java
@@ -14,12 +14,19 @@ public class Board {
 		this.height = height;
 	}
 	
-	public ArrayList<Card> genPlayers(int numPlayers)
+	public ArrayList<Player> genPlayers(int numPlayers)
 	{
 		ArrayList<Player> players = new ArrayList<Player>();
 		Random rand = new Random();
 		int r1 = rand.nextInt(6);
 		
 		return players;
+	}
+	
+	public ArrayList<Card> genAns()
+	{
+		ArrayList<Card> cards = new ArrayList<Card>();
+		
+		return cards;
 	}
 }
