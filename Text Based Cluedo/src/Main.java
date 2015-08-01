b0b02e6fe055ca521b382b434d9e3a064d908f5c
James Barfoote <james.barfoote@gmail.com>
2015-08-01 15:14:02 +1200

    Started on main class

--- Text Based Cluedo/src/Main.java
+++ Text Based Cluedo/src/Main.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
 public class Main {
+	
+	public static void playGame()
+	{
+		int h = 25;
+		int w = 25;
+		ArrayList<Player> players = new ArrayList<Player>();
+		ArrayList<Card> answer = new ArrayList<Card>();
+		Board b = new Board(answer, players, h, w);
+	}
+		
+	public static void main(String [ ] args)
+	{
+		playGame();
+	}
 
 }
