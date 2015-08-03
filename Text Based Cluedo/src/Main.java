c0748e7e1a47028012a1fdcc9b41f3896fb33eb7
CombuskenKid <Cameronporter95@gmail.com>
2015-08-04 11:29:14 +1200

    Fixed mismatch

--- Text Based Cluedo/src/Main.java
+++ Text Based Cluedo/src/Main.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -7,15 +6,26 @@ import java.util.Scanner;
 public class Main {
 	private static boolean finished = false;
 
-	public static void createGame(Scanner scan){
+	public static void createGame(Scanner scan) {
 		System.out.println("Welcome to Cluedo!");
-		int numP = 0;
+		String numP = "";
+		int numPlayers = 0;
 
-		//ask for how many players are playing
+		// ask for how many players are playing
 		System.out.println("How many players?");
-		numP = scan.nextInt();
+		numP = scan.next();
+		while(true){
+			if(isInteger(numP)){
+				numPlayers = Integer.parseInt(numP); 
+				if((numPlayers > 0) && (numPlayers<7)){
+					break;
+				}
+			}
+			System.out.println("Must be between 1 and 6 players");
+			numP = scan.next();
+		}
 
-		Board b = new Board(numP);
+		Board b = new Board(numPlayers);
 
 		int playerNum = 0;
 
@@ -23,40 +33,40 @@ public class Main {
 	}
 
 	/**
-	 * Loop through all the players while the game hasn't been won.
-	 * If a player gets eliminated, break the loop then remove 
-	 * the player and start the loop again from where it left off.
+	 * Loop through all the players while the game hasn't been won. If a player
+	 * gets eliminated, break the loop then remove the player and start the loop
+	 * again from where it left off.
+	 * 
 	 * @param players
 	 * @param scan
 	 * @param b
-	 * @param playerNum - the player before the current
+	 * @param playerNum
+	 *            - the player before the current
 	 */
-	public static void playGame(Scanner scan, Board b, int playerNum){
-		
-		playerNum = (playerNum % b.getPlayers().size()) + 1;
-		Player currentPlayer = b.getPlayers().get(playerNum-1);
-		Player eliminatedPlayer = null;
-		while (finished == false) {
+	public static void playGame(Scanner scan, Board b, int playerNum) {
 
-			   
-			
+		playerNum = (playerNum % b.getPlayers().size()) + 1;
+		Player currentPlayer = b.getPlayers().get(playerNum - 1);
+		Player eliminatedPlayer = null;
+		while (finished == false) {		
 			System.out.println("");
-			System.out.println("You are "+ currentPlayer.getName());
-			//			System.out.println("Number of player:" + b.getPlayers().size());
-			//			System.out.println("Size of hand is: " + currentPlayer.getCards().size());
+			System.out.println("You are " + currentPlayer.getName());
+			// System.out.println("Number of player:" + b.getPlayers().size());
+			// System.out.println("Size of hand is: " +
+			// currentPlayer.getCards().size());
 			Room r = null;
-			//calculate distances	
+			// calculate distances
 			currentPlayer.calculateDistances(b);
-			//System.out.println("Calculated distances");
+			// System.out.println("Calculated distances");
 
 			Map<Room, Integer> rDist = currentPlayer.getRoomDist();
 			int i = 0;
-			//look at room distances map and print out each entry
+			// look at room distances map and print out each entry
 			ArrayList<Room> rooms = new ArrayList<Room>();
-			//	System.out.println("rDist size = " + rDist.size());
+			// System.out.println("rDist size = " + rDist.size());
 			System.out.println("");
 			System.out.println("Type the number of the room you wish to move towards");
-			for(Entry<Room, Integer> e: rDist.entrySet()){
+			for (Entry<Room, Integer> e : rDist.entrySet()) {
 				Room cr = e.getKey();
 				int dist = e.getValue();
 				rooms.add(cr);
@@ -65,34 +75,33 @@ public class Main {
 				i++;
 			}
 
-			//roll dice
+			// roll dice
 			int diceNum = currentPlayer.rollDice();
 			System.out.println("");
 			System.out.println("You rolled " + diceNum);
 
-			//give option for movement (dont have to)
+			// give option for movement (dont have to)
 			System.out.println("Where do you want to move to?");
 			int numChoice = scan.nextInt();
 
-			//update location
+			// update location
 			r = rooms.get(numChoice);
 			currentPlayer.updateLocation(r);
 			scan.useDelimiter(System.getProperty("line.separator"));
-			//make a guess option (suggestion, accusation OR nothing)
+			// make a guess option (suggestion, accusation OR nothing)
 			System.out.println("What would you like to do?");
 
 			int distToRoom = rDist.get(r);
-			if(distToRoom > diceNum)
-			{
+			if (distToRoom > diceNum) {
 				System.out.println("1 - Accusation");
 				System.out.println("2 - Nothing");
 				int option = scan.nextInt();
 
-				if(option == 1) { 
+				if (option == 1) {
 
 					System.out.println("Please type the 3 cards that you are guessing on a new line");
 					System.out.println("In this order: Room, Weapon, Character");
-					//get cards from typed input on new line
+					// get cards from typed input on new line
 					String roomName = scan.next();
 					int index = b.getRoomNames().indexOf(roomName);
 					Room guessRoom = b.getRooms().get(index);
@@ -106,19 +115,18 @@ public class Main {
 
 					ArrayList<Card> guessHand = new ArrayList<Card>();
 					guessHand.add(guessRoom);
-					guessHand.add(guessWeapon);					
+					guessHand.add(guessWeapon);
 					guessHand.add(guessCharacter);
 
-					//create a guess hand
+					// create a guess hand
 					boolean opt = false;
 
 					Guess guess = new Guess(opt, guessHand, currentPlayer);
 
-					if(guess.getEliminatedPlayer()!=null){
+					if (guess.getEliminatedPlayer() != null) {
 						eliminatedPlayer = guess.getEliminatedPlayer();
 						break;
-					}
-					else if (guess.hasWon()) { 
+					} else if (guess.hasWon()) {
 						finished = true;
 						System.out.println("Congratulations " + currentPlayer.getName() + " you have won!");
 						return;
@@ -126,45 +134,38 @@ public class Main {
 
 				}
 
-
-			}
-			else
-			{
+			} else {
 
 				System.out.println("1 - Suggestion");
 				System.out.println("2 - Accusation");
 				System.out.println("3 - Nothing");
 				int option = scan.nextInt();
-				if(option == 1 || option == 2) { 
+				if (option == 1 || option == 2) {
 
 					System.out.println("Please type the 3 cards that you are guessing on a new line");
 					System.out.println("In this order: Room, Weapon, Character");
-					//get cards from typed input on new line
-
+					// get cards from typed input on new line
 
 					ArrayList<Card> guessHand = createGuess(scan, b);
-					while(guessHand == null)
-					{
+					while (guessHand == null) {
 						guessHand = createGuess(scan, b);
 					}
-					for(Card c : guessHand)
-					{
+					for (Card c : guessHand) {
 						System.out.println(c.getName());
 					}
 
-					//create a guess hand
+					// create a guess hand
 					boolean opt = false;
-					if(option == 1){
+					if (option == 1) {
 						opt = true;
 					}
 
 					Guess guess = new Guess(opt, guessHand, currentPlayer);
 
-					if(guess.getEliminatedPlayer()!=null){
+					if (guess.getEliminatedPlayer() != null) {
 						eliminatedPlayer = guess.getEliminatedPlayer();
 						break;
-					}
-					else if (guess.hasWon()) { 
+					} else if (guess.hasWon()) {
 						finished = true;
 						System.out.println("Congratulations " + currentPlayer.getName() + " you have won!");
 						return;
@@ -173,65 +174,66 @@ public class Main {
 				}
 			}
 			playerNum = (playerNum % b.getPlayers().size()) + 1;
-			currentPlayer = b.getPlayers().get(playerNum-1);
+			currentPlayer = b.getPlayers().get(playerNum - 1);
 		}
 
 		playerNum = (playerNum % Board.players.size()) - 1;
 		Board.players.remove(eliminatedPlayer);
-		while(b.getPlayers().size() > 1){
+		while (b.getPlayers().size() > 1) {
 			playGame(scan, b, playerNum);
 		}
 	}
 
-	private static ArrayList<Card> createGuess(Scanner scan, Board b)
-	{
+	private static ArrayList<Card> createGuess(Scanner scan, Board b) {
 		String roomName = scan.next();
 		int index = b.getRoomNames().indexOf(roomName);
 		Room guessRoom = null;
-		if(index != -1){ 
+		if (index != -1) {
 			guessRoom = b.getRooms().get(index);
-		}
-		else
-		{
+		} else {
 			System.out.println("Room name was incorrect, please type the 3 cards again");
 			return null;
 		}
 
 		int indexW = b.getWeaponNames().indexOf(scan.next());
 		Weapon guessWeapon = null;
-		if(indexW != -1){ 
+		if (indexW != -1) {
 			guessWeapon = b.getWeapons().get(indexW);
-		}
-		else
-		{
+		} else {
 			System.out.println("Weapon name was incorrect, please type the 3 cards again");
 			return null;
 		}
 
-
 		String characterN = scan.next();
 		int indexC = b.getCharacterNames().indexOf(characterN);
 		Character guessCharacter = null;
-		if(indexC != -1){ 
+		if (indexC != -1) {
 			guessCharacter = b.getCharacters().get(indexC);
-		}
-		else
-		{
+		} else {
 			System.out.println("Character name was incorrect, please type the 3 cards again");
 			return null;
 		}
 
-
 		ArrayList<Card> guessHand = new ArrayList<Card>();
 		guessHand.add(guessRoom);
-		guessHand.add(guessWeapon);		
+		guessHand.add(guessWeapon);
 		guessHand.add(guessCharacter);
 
 		return guessHand;
 
 	}
+	
+	private static boolean isInteger(String s) {
+		  try { 
+		      Integer.parseInt(s); 
+		   } catch(NumberFormatException e) { 
+		      return false; 
+		   }
+		   // only got here if we didn't return false
+		   return true;
+		}
 
-	public static void main(String [ ] args){
+	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		createGame(scan);
 	}
