6c693aa27bda9f64ff7deb18dd296f757c8a8125
CombuskenKid <Cameronporter95@gmail.com>
2015-08-04 15:31:11 +1200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Text Based Cluedo/bin/Main.class

--- Text Based Cluedo/src/Guess.java
+++ Text Based Cluedo/src/Guess.java
@@ -64,6 +64,7 @@ public class Guess {
 			}
 		} else {
 			System.out.println("No player had any of the suggested cards");
+			return;
 		}
 		System.out.println("Discovered card was: " + discoveredCard.getName());
 	}
